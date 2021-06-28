1e4177da89e909e05cae6fdeec94d7b80c763762
Nathan Darrow <nathanadarrow@gmail.com>
2021-06-28 16:26:28 -0700

    Login Works!w

--- routes/signup.js
+++ routes/signup.js
@@ -6,56 +6,60 @@ const { asyncHandler, logInUser, checkSessionToken } = require("../util");
 const db = require("../db/models");
 const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+const { render } = require("../app");
 
 const signupValidators = [
-	check("email")
-		.exists({ checkFalse: true })
-		.withMessage("Please provide a value for email")
-		.normalizeEmail()
-		.isEmail()
-		.withMessage("Please provide a valid email"),
-	check("username")
-		.exists({ checkFalse: true })
-		.withMessage("Please provide a value for username"),
-	check("password")
-		.matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i")
-		.withMessage(
-			"Please provide a password that is at least 8 characters long, at least one upper case letter, at least one lower case letter, at least one special character"
-		),
+  check("email")
+    .exists({ checkFalse: true })
+    .withMessage("Please provide a value for email")
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Please provide a valid email"),
+  check("username")
+    .exists({ checkFalse: true })
+    .withMessage("Please provide a value for username"),
+  check("password")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+    .withMessage(
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+    ),
 ];
 
-router.get("/signup", function (req, res, next) {
-	const { username, email, password, confirmedPassword } = req.body;
+router.get("/", function (req, res, next) {
+  const { username, email, password, confirmedPassword } = req.body;
+  res.render("signup");
 });
 
 router.post(
-	"/signup",
-	signupValidators,
-	asyncHandler(async function (req, res, next) {
-		const { username, email, password, confirmedPassword } = req.body;
+  "/",
+  signupValidators,
+  asyncHandler(async function (req, res, next) {
+    const { username, email, password, confirmedPassword } = req.body;
 
-		if (password !== confirmedPassword) {
-			return res.render("signup", {});
-		}
+    if (password !== confirmedPassword) {
+      return res.render("signup", {});
+    }
 
-		let errors = [];
-		const validatorErrors = validationResult(req);
+    let errors = [];
+    const validatorErrors = validationResult(req);
 
-		if (validatorErrors.isEmpty()) {
-			const hashedPassword = await bcrypt.hash(password, 10);
-			// user.hashedPassword = hashedPassword;
-			const user = await db.User.create({
-				username,
-				email,
-				hashedPassword,
-			});
-			logInUser(req, user);
-			return res.redirect("/");
-		} else {
-			errors = validatorErrors.array().map((error) => error.msg);
-		}
-
-		res.render("signup", { username, password, errors });
-	})
+    if (validatorErrors.isEmpty()) {
+      console.log(validatorErrors.isEmpty());
+      const hashedPassword = await bcrypt.hash(password, 10);
+      // user.hashedPassword = hashedPassword;
+      const user = await db.User.create({
+        username,
+        email,
+        hashedPassword,
+      });
+      logInUser(req, user);
+      return res.redirect("/");
+    } else {
+      console.log("validatorErrors is not empty");
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
+    console.log("Prepare to render!");
+    res.render("signup", { username, password, errors });
+  })
 );
 module.exports = router;
