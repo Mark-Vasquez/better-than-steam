7616acf129f2d1aeb8781a2f7d183ae0ff2048c3
kevjinx <kevin415mei@gmail.com>
2021-06-28 11:01:01 -0700

    add user model

---
+++ db/models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING(50),
+      validate: {
+        notNull: true,
+      }
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      validate: {
+        notNull: true,
+        isEmail: true,
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      validate: {
+        notNull: true,
+      }
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
