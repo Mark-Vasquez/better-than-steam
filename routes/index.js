d85f8a9995a48f0597b5997c76f9a4e962058aaa
kevjinx <kevin415mei@gmail.com>
2021-06-29 16:21:16 -0700

    add barebone mixins

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
+
+
+const testGameInfo1 = {
+  title: 'testTItle',
+  snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, officia.',
+  comments: '42',
+  logo: 'https://png.pngtree.com/png-vector/20190507/ourmid/pngtree-blue-samurai-head-esports-logo-png-image_1026489.jpg',
+  genre: 'yeeeet it',
+  votes: '42',
+  publisher: 'yeeters'
+}
+const testGameInfo2 = {
+  title: 'testTItle',
+  snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, officia.',
+  comments: '42',
+  logo: 'https://png.pngtree.com/png-vector/20190507/ourmid/pngtree-blue-samurai-head-esports-logo-png-image_1026489.jpg',
+  genre: 'yeeeet it',
+  votes: '42',
+  publisher: 'yeeters'
+}
+const testGameInfo3 = {
+  title: 'testTItle',
+  snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, officia.',
+  comments: '42',
+  logo: 'https://png.pngtree.com/png-vector/20190507/ourmid/pngtree-blue-samurai-head-esports-logo-png-image_1026489.jpg',
+  genre: 'yeeeet it',
+  votes: '42',
+  publisher: 'yeeters'
+}
+
+const gameData = [testGameInfo1, testGameInfo2, testGameInfo3]
+
+
+
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+  res.render('index', {
+    title: 'a/A Express Skeleton Home',
+    gameData
+  });
 });
 
 module.exports = router;
