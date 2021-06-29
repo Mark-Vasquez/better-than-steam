f075f6c6b58d0f2616c42976381ca93ec52b5ff9
Mark Vasquez <markjasonvasquez1@gmail.com>
2021-06-29 10:55:45 -0700

    refactor login/logout/signup into users.js

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const express = require("express");
 const csrf = require("csurf");
 const csrfProtection = csrf({ cookie: true });
 const router = express.Router();
-const { asyncHandler, logInUser, checkSessionToken } = require("../util");
+const { asyncHandler, logInUser, checkSessionToken, logOutUser } = require("../util");
 const db = require("../db/models");
 const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 //app using this usersRouter for /users routes
 /* GET users listing. */
@@ -13,12 +14,6 @@ router.get("/", function (req, res, next) {
 	res.send("respond with a resource");
 });
 
-router.get("/login", csrfProtection, checkSessionToken, function (req, res, next) {
-
-	res.render("login", {
-		csrfToken: req.csrfToken(),
-	});
-});
 
 router.post(
 	"/login",
@@ -31,6 +26,106 @@ router.post(
 		});
 	})
 )	
+
+router.post('/logout', (req, res) => {
+	logOutUser(req);
+	res.redirect('/');
+});
+
+router.get("/login", csrfProtection, checkSessionToken, function (req, res, next) {
+	res.render("login", {
+		csrfToken: req.csrfToken(),
+	});
+});
+
+router.post(
+	"/",
+	csrfProtection,
+	asyncHandler(async function (req, res, next) {
+
+		const { username, password } = req.body;
+
+		const thisUser = await db.User.findOne({
+			where: { username },
+		});
+
+		if (thisUser) {
+			const passwordMatch = await bcrypt.compare(
+				password,
+				thisUser.hashedPassword.toString()
+			);
+
+			if (passwordMatch) {
+				logInUser(req, thisUser);
+				return res.redirect("/");
+			} else {
+				//if password doesn't match
+				res.render("login", {
+					username,
+					email,
+					csrfToken: req.csrfToken(),
+				});
+			}
+		} else {
+			//if email doesn't exists
+		}
+	})
+);
+
+const signupValidators = [
+  check("email")
+    .exists({ checkFalse: true })
+    .withMessage("Please provide a value for email")
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Please provide a valid email"),
+  check("username")
+    .exists({ checkFalse: true })
+    .withMessage("Please provide a value for username"),
+  check("password")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+    .withMessage(
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+    ),
+];
+
+router.get("/signup", function (req, res, next) {
+  const { username, email, password, confirmedPassword } = req.body;
+  res.render("signup");
+});
+
+router.post(
+  "/signup",
+  signupValidators,
+  asyncHandler(async function (req, res, next) {
+    const { username, email, password, confirmedPassword } = req.body;
+
+    if (password !== confirmedPassword) {
+      return res.render("signup", {});
+    }
+
+    let errors = [];
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      console.log(validatorErrors.isEmpty());
+      const hashedPassword = await bcrypt.hash(password, 10);
+      // user.hashedPassword = hashedPassword;
+      const user = await db.User.create({
+        username,
+        email,
+        hashedPassword,
+      });
+      logInUser(req, user);
+      return res.redirect("/");
+    } else {
+      console.log("validatorErrors is not empty");
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
+    console.log("Prepare to render!");
+    res.render("signup", { username, password, errors });
+  })
+);
 
 
 module.exports = router;
