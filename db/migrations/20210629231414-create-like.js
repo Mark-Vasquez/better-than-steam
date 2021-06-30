a8701d5dabcb0cbd287c1f5943e1077d32bba4ca
Justice Martin <jujmart12@gmail.com>
2021-06-30 12:46:12 -0400

    added composite unique index for likes table

--- db/migrations/20210629231414-create-like.js
+++ db/migrations/20210629231414-create-like.js
@@ -1,37 +1,46 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Likes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: "Users" },
-        unique: "actions_unique"
-      },
-      commentId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: "Comments" },
-        unique: "actions_unique"
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    },
-  );
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Likes');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable(
+			"Likes",
+			{
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER,
+				},
+				userId: {
+					allowNull: false,
+					type: Sequelize.INTEGER,
+					references: { model: "Users" },
+					unique: "unique-like",
+				},
+				commentId: {
+					allowNull: false,
+					type: Sequelize.INTEGER,
+					references: { model: "Comments" },
+					unique: "unique-like",
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+				},
+			},
+			{
+				uniqueKeys: {
+					"unique-like": {
+						fields: ["userId", "commentId"],
+					},
+				},
+			}
+		);
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable("Likes");
+	},
 };
