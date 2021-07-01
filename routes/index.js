799309251e84fe5aa400df805de643e9c4c929fb
kevjinx <kevin415mei@gmail.com>
2021-07-01 09:29:49 -0700

    update votes with numbers

--- routes/index.js
+++ routes/index.js
@@ -3,47 +3,33 @@ const router = express.Router();
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 const db = require('../db/models')
 
-// const testGameInfo1 = {
-//   title: 'testTItle',
-//   snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, officia.',
-//   comments: '42',
-//   logo: 'https://png.pngtree.com/png-vector/20190507/ourmid/pngtree-blue-samurai-head-esports-logo-png-image_1026489.jpg',
-//   genre: 'yeeeet it',
-//   votes: '42',
-//   publisher: 'yeeters'
-// }
-// const testGameInfo2 = {
-//   title: 'testTItle',
-//   snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, officia.',
-//   comments: '42',
-//   logo: 'https://png.pngtree.com/png-vector/20190507/ourmid/pngtree-blue-samurai-head-esports-logo-png-image_1026489.jpg',
-//   genre: 'yeeeet it',
-//   votes: '42',
-//   publisher: 'yeeters'
-// }
-// const testGameInfo3 = {
-//   title: 'testTItle',
-//   snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, officia.',
-//   comments: '42',
-//   logo: 'https://png.pngtree.com/png-vector/20190507/ourmid/pngtree-blue-samurai-head-esports-logo-png-image_1026489.jpg',
-//   genre: 'yeeeet it',
-//   votes: '42',
-//   publisher: 'yeeters'
-// }
 
-/* GET home page. */
 
 router.get('/', asyncHandler(async function(req, res, next) {
 
   const gameData = await db.Game.findAll()
-  
+  const allUpvotes = await db.Upvote.findAll()
+
+  const gameVote = {}
+
+  for (let i = 0; i < allUpvotes.length; i++) {
+    const vote = allUpvotes[i].dataValues
+    const gameId = vote.gameId
+
+    if (gameVote[gameId]) {
+      gameVote[gameId]++
+    } else {
+      gameVote[gameId] = 1
+    }
+
+  }
 
   res.render('index', {
     title: 'Better Than Steam.',
-    gameData
+    gameData,
+    gameVote
   });
 }));
-
 
 
 module.exports = router;
