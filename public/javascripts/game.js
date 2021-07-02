f473439baa984f7ca63169192fad5c7b3f58d9bc
Justice Martin <jujmart12@gmail.com>
2021-07-02 00:00:20 -0400

    Added delete comment functionality

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,8 +1,90 @@
+const postCommentButton = document.getElementById("post-comment-button");
+const commentField = document.querySelector(".comment-content");
+const errorUl = document.getElementById("errors-ul");
+const commentContainer = document.querySelector(".comments-container");
+
+function populateComments(res) {
+	if (res.errors) {
+		errorUl.innerHTML = "";
+		res.errors.forEach((error) => {
+			const newError = document.createElement("li");
+			newError.innerHTML = error;
+			errorUl.appendChild(newError);
+		});
+	} else {
+		commentContainer.innerHTML = "";
+		res.comments.forEach((comment) => {
+			const contentWrapper = document.createElement("div");
+			contentWrapper.classList.add("comments__content-wrapper");
+
+			const contentUser = document.createElement("div");
+			contentUser.classList.add("comments__content__user");
+
+			const avatar = document.createElement("img");
+			avatar.classList.add("comments__content__user-avatar");
+			avatar.src =
+				"https://www.pikpng.com/pngl/b/279-2797047_user-avatar-icon-portable-network-graphics-clipart.png";
+
+			const textWrapper = document.createElement("div");
+			textWrapper.classList.add("comments__content__text-wrapper");
+
+			const textUsername = document.createElement("p");
+			textUsername.classList.add("comments__content__text-username");
+			textUsername.innerHTML = comment.User.username;
+
+			const text = document.createElement("p");
+			text.classList.add("comments__content__text");
+			text.innerHTML = comment.content;
+
+			const newEditButton = document.createElement("button");
+			newEditButton.classList.add("edit");
+			newEditButton.innerText = "Edit";
+			newEditButton.value = comment.id;
+
+			const newDeleteButton = document.createElement("button");
+			newDeleteButton.classList.add("delete");
+			newDeleteButton.innerText = "Delete";
+			newDeleteButton.value = comment.id;
+
+			if (
+				textUsername.innerHTML !== res.username &&
+				res.username !== "DemoUser"
+			) {
+				newEditButton.disabled = true;
+				newDeleteButton.disabled = true;
+			}
+
+			commentContainer.appendChild(contentWrapper);
+			contentWrapper.appendChild(contentUser);
+			contentUser.appendChild(avatar);
+			contentWrapper.appendChild(textWrapper);
+			textWrapper.appendChild(textUsername);
+			textWrapper.appendChild(text);
+			textWrapper.appendChild(newEditButton);
+			textWrapper.appendChild(newDeleteButton);
+
+			newDeleteButton.addEventListener("click", async (e) => {
+				e.preventDefault();
+				const url = document.URL;
+				const urlSplit = url.split("/");
+				const gameId = urlSplit[urlSplit.length - 1];
+
+				const commentId = e.target.value;
+				const resJson = await fetch(`/comments/${commentId}`, {
+					method: "DELETE",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ gameId }),
+				});
+				const res = await resJson.json();
+				populateComments(res);
+			});
+		});
+		commentField.value = "";
+	}
+}
+
 window.addEventListener("DOMContentLoaded", (event) => {
-	const postCommentButton = document.getElementById("post-comment-button");
-	const commentField = document.querySelector(".comment-content");
-	const errorUl = document.getElementById("errors-ul");
-	const commentContainer = document.querySelector(".comments-container");
+	let deleteButtons = document.querySelectorAll(".delete");
 
 	postCommentButton.addEventListener("click", async (e) => {
 		e.preventDefault();
@@ -17,63 +99,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
 			body: JSON.stringify({ content, gameId }),
 		});
 		const res = await resJson.json();
-		if (res.errors) {
-			errorUl.innerHTML = "";
-			res.errors.forEach((error) => {
-				const newError = document.createElement("li");
-				newError.innerHTML = error;
-				errorUl.appendChild(newError);
+		populateComments(res);
+	});
+
+	Array.from(deleteButtons).forEach((deleteButton) => {
+		deleteButton.addEventListener("click", async (e) => {
+			e.preventDefault();
+			const url = document.URL;
+			const urlSplit = url.split("/");
+			const gameId = urlSplit[urlSplit.length - 1];
+
+			const commentId = e.target.value;
+			const resJson = await fetch(`/comments/${commentId}`, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ gameId }),
 			});
-		} else {
-			commentContainer.innerHTML = "";
-			res.comments.forEach((comment) => {
-				const contentWrapper = document.createElement("div");
-				contentWrapper.classList.add("comments__content-wrapper");
-
-				const contentUser = document.createElement("div");
-				contentUser.classList.add("comments__content__user");
-
-				const avatar = document.createElement("img");
-				avatar.classList.add("comments__content__user-avatar");
-				avatar.src =
-					"https://www.pikpng.com/pngl/b/279-2797047_user-avatar-icon-portable-network-graphics-clipart.png";
-
-				const textWrapper = document.createElement("div");
-				textWrapper.classList.add("comments__content__text-wrapper");
-
-				const textUsername = document.createElement("p");
-				textUsername.classList.add("comments__content__text-username");
-				textUsername.innerHTML = comment.User.username;
-
-				const text = document.createElement("p");
-				text.classList.add("comments__content__text");
-				text.innerHTML = comment.content;
-
-				const newEditButton = document.createElement("button");
-				newEditButton.classList.add("edit");
-				newEditButton.innerText = "Edit";
-				newEditButton.value = comment.id;
-
-				const newDeleteButton = document.createElement("button");
-				newDeleteButton.classList.add("delete");
-				newDeleteButton.innerText = "Delete";
-				newDeleteButton.value = comment.id;
-
-				if (textUsername.innerHTML !== res.username) {
-					newEditButton.disabled = true;
-					newDeleteButton.disabled = true;
-				}
-
-				commentContainer.appendChild(contentWrapper);
-				contentWrapper.appendChild(contentUser);
-				contentUser.appendChild(avatar);
-				contentWrapper.appendChild(textWrapper);
-				textWrapper.appendChild(textUsername);
-				textWrapper.appendChild(text);
-				textWrapper.appendChild(newEditButton);
-				textWrapper.appendChild(newDeleteButton);
-			});
-			commentField.value = "";
-		}
+			const res = await resJson.json();
+			populateComments(res);
+		});
 	});
 });
--- routes/comments.js
+++ routes/comments.js
@@ -5,59 +5,78 @@ const db = require("../db/models");
 const { check, validationResult } = require("express-validator");
 
 const commentValidators = [
-  check("content")
-    .exists({ checkFalsy: true })
-    .withMessage("Please type a comment before submitting."),
+	check("content")
+		.exists({ checkFalsy: true })
+		.withMessage("Please type a comment before submitting."),
 ];
 
 router.post(
-  "/",
-  commentValidators,
-  asyncHandler(async (req, res) => {
-    const validatorErrors = validationResult(req);
-    let errors = [];
-    if (validatorErrors.isEmpty() && req.session.auth) {
-      const userId = req.session.auth.user.id;
-	  const user = await db.User.findByPk(userId)
-	  const username = user.username;
-      const gameId = parseInt(req.body.gameId, 10);
-      const content = req.body.content;
-      await db.Comment.create({ content, gameId, userId });
-      const comments = await db.Comment.findAll({
-        where: { gameId },
-        order: [["createdAt", "DESC"]],
-        include: db.User,
-      });
-      await res.json({ comments, username });
-    } else if (req.session.auth) {
-      errors = validatorErrors.array().map((error) => error.msg);
-      await res.json({ errors });
-    } else {
-      errors = ["Please sign in to post a comment."];
-      await res.json({ errors });
-    }
-  })
+	"/",
+	commentValidators,
+	asyncHandler(async (req, res) => {
+		const validatorErrors = validationResult(req);
+		let errors = [];
+		if (validatorErrors.isEmpty() && req.session.auth) {
+			const userId = req.session.auth.user.id;
+			const user = await db.User.findByPk(userId);
+			const username = user.username;
+			const gameId = parseInt(req.body.gameId, 10);
+			const content = req.body.content;
+			await db.Comment.create({ content, gameId, userId });
+			const comments = await db.Comment.findAll({
+				where: { gameId },
+				order: [["createdAt", "DESC"]],
+				include: db.User,
+			});
+			await res.json({ comments, username });
+		} else if (req.session.auth) {
+			errors = validatorErrors.array().map((error) => error.msg);
+			await res.json({ errors });
+		} else {
+			errors = ["Please sign in to post a comment."];
+			await res.json({ errors });
+		}
+	})
 );
 
 router.put(
-  "/:id",
-  commentValidators,
-  asyncHandler(async (req, res) => {
-    const validatorErrors = validationResult(req);
-    let errors = [];
-    const comment = await db.Comment.findByPk(req.params.id, {
-      include: db.User,
-    });
-    if (validatorErrors.isEmpty()) {
-      const content = req.body.content;
-	  comment.content = content;
-	  await comment.save();
-      await res.json({ comment });
-    } else {
-      errors = validatorErrors.array().map((error) => error.msg);
-      await res.json({ errors });
-    }
-  })
+	"/:id",
+	commentValidators,
+	asyncHandler(async (req, res) => {
+		const validatorErrors = validationResult(req);
+		let errors = [];
+		const comment = await db.Comment.findByPk(req.params.id, {
+			include: db.User,
+		});
+		if (validatorErrors.isEmpty()) {
+			const content = req.body.content;
+			comment.content = content;
+			await comment.save();
+			await res.json({ comment });
+		} else {
+			errors = validatorErrors.array().map((error) => error.msg);
+			await res.json({ errors });
+		}
+	})
+);
+
+router.delete(
+	"/:id",
+	asyncHandler(async (req, res) => {
+		const comment = await db.Comment.findByPk(req.params.id);
+		await comment.destroy();
+		const userId = req.session.auth.user.id;
+		const user = await db.User.findByPk(userId);
+		const username = user.username;
+		const gameId = parseInt(req.body.gameId, 10);
+		const comments = await db.Comment.findAll({
+			where: { gameId },
+			order: [["createdAt", "DESC"]],
+			include: db.User,
+		});
+
+		await res.json({ comments, username });
+	})
 );
 
 module.exports = router;
